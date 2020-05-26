72816c18842c753079be0509d4144c07d002993d
Dimas Ragil Triatmaja <dynastymasra@gmail.com>
2020-05-26 12:57:26 +0700

    Add const

---
+++ config/const.go
@@ -0,0 +1,25 @@
+package config
+
+const (
+	ServiceName = "Cartographer"
+	Version     = "1.2.0"
+
+	// Service port address
+	envServerPort = "SERVER_PORT"
+
+	// Logger config
+	envLoggerFormat = "LOGGER_FORMAT"
+	envLoggerLevel  = "LOGGER_LEVEL"
+
+	// Neo4J config
+	envNeo4JAddress     = "NEO4J_ADDRESS"
+	envNeo4JUsername    = "NEO4J_USERNAME"
+	envNeo4JPassword    = "NEO4J_PASSWORD"
+	envNeo4JMaxConnPool = "NEO4J_MAX_CONN_POOL"
+	envNeo4JEncrypted   = "NEO4J_ENCRYPTED"
+	envNeo4JLogEnabled  = "NEO4J_LOG_ENABLED"
+	envNeo4JLogLevel    = "NEO4J_LOG_LEVEL"
+
+	Limit  = 25
+	Offset = 0
+)
